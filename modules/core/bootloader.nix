aba5c982ee11e0fac6f6df40227fd545d97876c7
abdyllaan <64682935+abdyllaann@users.noreply.github.com>
2025-05-27 19:41:03 +0700

    Update bootloader.nix

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,6 +2,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.configurationLimit = 10;
   time.timeZone = "Asia/Jakarta";
   boot.supportedFilesystems = [ "cifs" ];
 }
