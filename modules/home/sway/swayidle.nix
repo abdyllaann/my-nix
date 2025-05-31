16cbe3cf056ab339eca6deeaf4e298a0c4691812
abdylla adhiyasa <adhiyasaabdylla@gmail.com>
2025-06-01 00:05:01 +0700

    add swayidle

--- modules/home/sway/swayidle.nix
+++ modules/home/sway/swayidle.nix
@@ -1,27 +1,27 @@
+{pkgs, ...}:
 {
-  config,
-  pkgs,
-  ...
-}: {
-  services.swayidle = {
-    enable = true;
-    package = pkgs.swayidle;
-    events = [
-      {
-        event = "before-sleep";
-        command = "swaylock";
-      }
-      {
-        event = "lock";
-        command = "swaylock";
-      }
-    ];
-    timeouts = [
-      {
-        timeout = 3;
-        command = "swaylock -fF";
-      }
-    ];
-  };
+  home.packages = with pkgs; [ swaylock-effects ];
 
+  systemd.user.services.swayidle = {
+    Unit = {
+      Description = "Idle manager for Wayland";
+      After = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = ''
+        ${pkgs.swayidle}/bin/swayidle -w \
+          timeout 300 '${pkgs.swaylock-effects}/bin/swaylock --effect-blur 7x5 --clock --indicator' \
+          timeout 600 '${pkgs.sway}/bin/swaymsg "output * dpms off"' \
+            resume '${pkgs.sway}/bin/swaymsg "output * dpms on"' \
+          before-sleep '${pkgs.swaylock-effects}/bin/swaylock --effect-blur 7x5 --clock --indicator' \
+          lock '${pkgs.swaylock-effects}/bin/swaylock --effect-blur 7x5 --clock --indicator'
+      '';
+      Restart = "always";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
